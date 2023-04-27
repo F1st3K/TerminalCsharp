1927644f6511a3d7a451b637050e5d5fbad733fe
F1st3K <n1k0s04k@gmail.com>
2023-04-27 11:54:49 +0300

    Create terminal and view

---
+++ Terminal/Terminal/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Terminal
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            await System.Threading.Tasks.Task.Run(() => Work());
+        }
+
+        static void Work()
+        {
+            var view = new View();
+            view.Start();
+            while (true)
+            {
+                string[] commands = view.Run().Split(' ');
+                if (commands.Length == 0)
+                    continue;
+                if (commands[0] == "exit")
+                {
+                    view.Stop();
+                    break;
+                }
+
+            }
+        }
+    }
+}
---
+++ Terminal/Terminal/ViewFiles.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace Terminal
+{
+    internal sealed class ViewFiles
+    {
+        public void Start()
+        {
+            Console.WriteLine("Terminal is started:");
+        }
+
+        public string Run()
+        {
+            string view = "┌─[-]─[" + Environment.UserName + "@" + Environment.UserDomainName + "]" +
+                    "─[" + Directory.GetCurrentDirectory() + "]\n└──$";
+            Console.Write(view);
+            return Console.ReadLine();
+        }
+
+        public void Output(string value)
+        {
+            Console.WriteLine(value);
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Terminal is stoped!");
+            Console.ReadKey();
+        }
+    }
+}
