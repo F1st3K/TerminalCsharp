8485fc77717efc552f39bb54a93d0acbbcf0922b
F1st3K <n1k0s04k@gmail.com>
2023-05-02 18:51:44 +0300

    copypast help information

--- Terminal/Terminal/Program.cs
+++ Terminal/Terminal/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Reflection;
-
-namespace Terminal
+﻿namespace Terminal
 {
     class Program
     {
@@ -15,25 +13,31 @@ namespace Terminal
             var executor = new Executor();
             view.Start();
             while (true)
-            {
-                string[] commands = view.Run().Split(new char[] {' '}, System.StringSplitOptions.RemoveEmptyEntries);
-                if (commands.Length == 0)
-                    continue;
-                switch (commands[0])
+                try
                 {
-                    case "exit":
-                        view.Stop();
-                        return;
-                    case "clear":
-                        view.Clear();
+                    string[] commands = view.Run().Split(new char[] {' '}, System.StringSplitOptions.RemoveEmptyEntries);
+                    if (commands.Length == 0)
                         continue;
-                    default:
-                        break;
+                    switch (commands[0])
+                    {
+                        case "exit":
+                            view.Stop();
+                            return;
+                        case "clear":
+                            view.Clear();
+                            continue;
+                        default:
+                            break;
+                    }
+                    if (executor.CommandIsExist(commands[0]))
+                        view.Output(executor.RunCommand(commands));
+                    else view.CommandNotFound(commands[0]);
                 }
-                if (executor.CommandIsExist(commands[0]))
-                    view.Output(executor.RunCommand(commands));
-                else view.CommandNotFound(commands[0]);
-            }
+                catch
+                {
+                    view.Output("Terminal: ooops..");
+                    continue;
+                }
         }
     }
 }
